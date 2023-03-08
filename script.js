b30720195e35e6b54208129e26ff2a1b2cd723b1
678etc <678etc@gmail.com>
2023-03-08 18:29:57 +0800

    20230308182954

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-function show(type)
-{
-    count = 0;
-    for (var key in methods) {
-        var row = document.getElementById(key);
-        if ((methods[key] &  type) != 0) {
-            row.style.display = '';
-            row.className = (count++ % 2) ? rowColor : altColor;
-        }
-        else
-            row.style.display = 'none';
-    }
-    updateTabs(type);
-}
-
-function updateTabs(type)
-{
-    for (var value in tabs) {
-        var sNode = document.getElementById(tabs[value][0]);
-        var spanNode = sNode.firstChild;
-        if (value == type) {
-            sNode.className = activeTableTab;
-            spanNode.innerHTML = tabs[value][1];
-        }
-        else {
-            sNode.className = tableTab;
-            spanNode.innerHTML = "<a href=\"javascript:show("+ value + ");\">" + tabs[value][1] + "</a>";
-        }
-    }
-}
-
 var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML='::selection{background-color:red;color:white; }::-moz-selection{background:red;color:white;}body{display:none}.highlight{background-color: yellow;}#key_nav{background-color:white;z-index:99999999;position:fixed;top:30px;right:0};';
